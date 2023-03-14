f1ce6e83ee1872dd64d5654c398378ba37fc98ac
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-03-14 23:08:50 +0100

    Update context provider type

--- context/ToDosContext.tsx
+++ context/ToDosContext.tsx
@@ -1,10 +1,10 @@
-import { createContext, FC, useContext } from "react";
+import { createContext, ReactNode, useContext } from "react";
 import { useToDos } from "../hooks/useToDos";
 
 type ToDosContext = ReturnType<typeof useToDos>;
 const Context = createContext({} as ToDosContext);
 
-export const ToDosContextProvider: FC = ({ children }) => (
+export const ToDosContextProvider = ({ children }: { children: ReactNode }) => (
   <Context.Provider value={useToDos()}>{children}</Context.Provider>
 );
 
